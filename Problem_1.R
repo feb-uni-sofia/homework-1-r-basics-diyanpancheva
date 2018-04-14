2916e2937f2329534a8ab4cc1a23255092639286
diyanpancheva <37041084+diyanpancheva@users.noreply.github.com>
2018-04-14 22:22:30 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,26 @@
+# Problem 1
 
+# a)
+x <- c(4,1,1,4) 
+
+# b)
+y <- c(1,4)
+
+# c)
+x-y
+#
+
+# d)
+s <- c(x,y)
+
+# e)
+rep(s,10)
+
+# f)
+rep(s, each=3)
+
+# g)
+seq(from=7, to=21)
+
+# h)
+length(7:21)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,36 @@
+# Problem 2
 
+# a)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+# b)
+xmax - xmin
+
+# c)
+mean(xmax)
+
+mean(xmin)
+
+# d)
+xmin[xmin<mean(xmin)]
+
+# e)
+xmax[xmax>mean(xmax)]
+
+# f)
+setNames(xmin, c("03Mon18", "04Tue18", "05Wed18", "04Thu18", "05Fri18", "06Sat18", "07Sun18"))
+setNames(xmax, c("03Mon18", "04Tue18", "05Wed18", "04Thu18", "05Fri18", "06Sat18", "07Sun18"))
+
+# g)
+temperatures <- data.frame(xmax, xmin)
+
+# h)
+temperatures$xminFahrenheit <- 9/5*xmin+32
+
+# i)
+Fahrenheit <- data.frame(xminFahrenheit)
+
+# j)
+New <- data.frame(Fahrenheit[1:5,])
